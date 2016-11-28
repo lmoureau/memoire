830e8c11a974c7d6daa70a9f27960ccf618179b3
Louis Moureaux <lmoureau@ulb.ac.be>
2016-11-28 11:01:51 +0100

    analysis: Fix calo cuts

--- analysis/calocut.lua
+++ analysis/calocut.lua
@@ -2,13 +2,13 @@
 if e.castor_energy > 9 then
   return false
 end
-if e.hcal.fp.t > 3 or e.hcal.bp.t > 3 then
+if e.hcal.fm.t > 3 or e.hcal.bp.t > 3 then
   return false
 end
-if e.hcal.ep.t > 1.95 or e.hcal.ep.t > 1.95 then
+if e.hcal.em.t > 1.95 or e.hcal.ep.t > 1.95 then
   return false
 end
-if e.hcal.bp.t > 1.18 or e.hcal.bp.t > 1.18 then
+if e.hcal.bm.t > 1.18 or e.hcal.bp.t > 1.18 then
   return false
 end
 
